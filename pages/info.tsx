9a0923ffd1eba12e4a1ea3179fcae098dec471ae
CristobalSg <111024562+CristobalSg@users.noreply.github.com>
2024-11-03 22:27:21 -0300

    fix: :art: fix
    fix

--- pages/info.tsx
+++ pages/info.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Typography, Card, Row, Col, Image } from 'antd';
+import React, { useState } from 'react';
+import { Typography, Card, Row, Col } from 'antd';
 import DynamicTitle from '@/components/TitleWithLinesStyle';
 
 const { Title } = Typography;
 
 const cardColors = ['#FFE0E0', '#E0FFE0', '#E0E0FF', '#E0F7FA'];
-const cardStyle = (index: number) => ({
+const cardStyle = (index: number, isHovered: boolean) => ({
   background: cardColors[index % cardColors.length],
-  boxShadow: '0 2px 8px rgba(0,0,0,0.05)',
+  boxShadow: isHovered ? '0 4px 16px rgba(0, 0, 0, 0.2)' : '0 2px 8px rgba(0, 0, 0, 0.05)', // Cambia la sombra según el hover
   borderRadius: '8px',
   marginBottom: '24px',
   height: '350px', // Establece una altura fija para las tarjetas
@@ -18,18 +18,22 @@ const resources = [
   {
     title: 'Ejercicios de movilidad',
     imgSrc: 'https://media-public.canva.com/qaBcU/MAF-OUqaBcU/1/tl.png',
+    href: "https://www.canva.com/design/DAGS6e2_NkY/nZe_rUvLnyKBSivrPbWTMQ/view?utm_content=DAGS6e2_NkY&utm_campaign=designshare&utm_medium=link&utm_source=editor"
   },
   {
     title: 'Estimulación cognitiva',
     imgSrc: 'https://media-public.canva.com/YOF4c/MAGLwaYOF4c/1/tl.png',
+    href: "https://www.canva.com/design/DAGS6e2_NkY/nZe_rUvLnyKBSivrPbWTMQ/view?utm_content=DAGS6e2_NkY&utm_campaign=designshare&utm_medium=link&utm_source=editor"
   },
   {
     title: 'Actividades básicas de la vida diaria',
     imgSrc: 'https://media-public.canva.com/WTBu0/MAFHpMWTBu0/1/tl.png',
+    href: "https://www.canva.com/design/DAGS6e2_NkY/nZe_rUvLnyKBSivrPbWTMQ/view?utm_content=DAGS6e2_NkY&utm_campaign=designshare&utm_medium=link&utm_source=editor"
   },
   {
     title: 'Estimulación polisensorial',
     imgSrc: 'https://media-public.canva.com/oIBOU/MAGMeooIBOU/1/tl.png',
+    href: "https://www.canva.com/design/DAGS6e2_NkY/nZe_rUvLnyKBSivrPbWTMQ/view?utm_content=DAGS6e2_NkY&utm_campaign=designshare&utm_medium=link&utm_source=editor"
   },
 ];
 
@@ -42,24 +46,35 @@ const SalePage = () => {
         paragraphText="A continuación ¡te enseñamos cuáles!"
       />
       <Row gutter={[16, 16]} justify="center">
-        {resources.map((resource, idx) => (
-          <Col key={idx} xs={24} sm={12} md={8} lg={6}>
-            <Card style={{ ...cardStyle(idx), height: '100%' }} hoverable>
-              {/* Contenedor de la imagen con altura fija */}
-              <div style={{ height: '200px', display: 'flex', justifyContent: 'center', marginBottom: '16px' }}>
-                <Image
-                  width={"100%"}
-                  height={"100%"}
-                  style={{ objectFit: 'contain' }}
-                  src={resource.imgSrc}
-                />
-              </div>
-              <Title level={3} style={{ textAlign: 'center', textTransform: 'uppercase', textDecoration: 'underline' }}>
-                {resource.title}
-              </Title>
-            </Card>
-          </Col>
-        ))}
+        {resources.map((resource, idx) => {
+          const [isHovered, setIsHovered] = useState(false); // Estado para el hover
+          
+          return (
+            <Col key={idx} xs={24} sm={12} md={8} lg={6}>
+              <a href={resource.href} target="_blank" rel="noopener noreferrer">
+                <Card
+                  style={cardStyle(idx, isHovered)} // Pasamos el estado de hover a la función de estilo
+                  onMouseEnter={() => setIsHovered(true)} // Cambiamos el estado al pasar el mouse
+                  onMouseLeave={() => setIsHovered(false)} // Restauramos el estado al salir el mouse
+                  hoverable
+                >
+                  {/* Contenedor de la imagen con altura fija */}
+                  <div style={{ height: '200px', display: 'flex', justifyContent: 'center', marginBottom: '16px' }}>
+                    <img
+                      width={"100%"}
+                      height={"100%"}
+                      style={{ objectFit: 'contain' }}
+                      src={resource.imgSrc}
+                    />
+                  </div>
+                  <Title level={3} style={{ textAlign: 'center', textTransform: 'uppercase', textDecoration: 'underline' }}>
+                    {resource.title}
+                  </Title>
+                </Card>
+              </a>
+            </Col>
+          );
+        })}
       </Row>
     </>
   );
